67dea865db7594cd34c1ee0cd1b884f5d0b52685
Code-With-Hassan <muhammadhassanshafique5@GMAIL.COM>
2024-09-09 00:20:58 +0500

    Add PrivilegesModel.cs

---
+++ Globe.Shared.Models/Privileges/PrivilegesModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Globe.Shared.Models.Privileges
+{
+    public class PrivilegesModel
+    {
+        /// <summary>
+        /// Gets or sets the Screen Id.
+        /// </summary>
+        [Required]
+        public long ScreenId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Screen Priviliges.
+        /// 0 = None, 1 = Admin, 2 = Custom, 3=ReadOnly
+        /// </summary>
+        [Required]
+        public int ScreenPriviliges { get; set; }
+    }
+}
